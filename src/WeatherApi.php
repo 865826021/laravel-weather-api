16f74b3f9d0604dcc89c22b688763b4e3fa1d533
falstack <icesilt@outlook.com>
2016-10-21 12:16:16 +0800

    init project

---
+++ src/WeatherApi.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Falstack\WeatherApi;
+
+class WeatherApi
+{
+    public function make($val, array $opts = [])
+    {
+        $salt = isset($opts['salt']) ? $opts['salt'] : '';
+
+        return hash('md5', $val . $salt);
+    }
+
+    public function check($val, $hashVal, array $opts = [])
+    {
+        $salt = isset($opts['salt']) ? $opts['salt'] : '';
+
+        return hash('md5', $val . $salt) == $hashVal;
+    }
+}
---
+++ src/WeatherApiServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Falstack\WeatherApi;
+
+use Illuminate\Support\ServiceProvider;
+
+class WeatherApiServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('weatherApi', function () {
+            return new WeatherApi();
+        });
+    }
+}
